cd194f479adf0c4a49377176adce8e65ed1ec7fd
mert-s <mert-sahan@hotmail.com>
2022-12-02 22:33:24 +0000

    1

---
+++ AdventOfCode2022/1/1.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+var sumCalsList = System.IO.File.ReadAllText(@"1/myInput.txt")
+    .Split("\n\n")
+    .Select(x => x.Split("\n").Where(x => x != "").Sum(int.Parse)).ToList();
+sumCalsList.Sort();
+
+Console.Out.WriteLine($"Max calories: {sumCalsList[^1]}");
+Console.Out.WriteLine($"Sum of top 3 calories: {sumCalsList.TakeLast(3).Sum()}");
