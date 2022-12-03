133885b32fe52588108a112722f10ede634c0099
mert-s <mert-sahan@hotmail.com>
2022-12-03 11:35:54 +0000

    Day 2, Part 2

--- Day2/2.cs
+++ Day2/2.cs
@@ -2,7 +2,7 @@
 
 var lines = System.IO.File.ReadAllLines(@"myInput.txt");
 
-var allCases = new Dictionary<string, int> {
+var allCasesPart1 = new Dictionary<string, int> {
     { "A X", 1+3 },
     { "A Y", 2+6 },
     { "A Z", 3 },
@@ -14,6 +14,22 @@ var allCases = new Dictionary<string, int> {
     { "C Z", 3+3 },
 };
 
-var total = lines.Sum(x => allCases[x]);
+var total = lines.Sum(x => allCasesPart1[x]);
 
-Console.Out.WriteLine($"{total}");
+Console.Out.WriteLine($"Part 1: {total}");
+
+var allCasesPart2 = new Dictionary<string, int> {
+    { "A X", 3 },
+    { "A Y", 1+3 },
+    { "A Z", 2+6 },
+    { "B X", 1 },
+    { "B Y", 2+3 },
+    { "B Z", 3+6 },
+    { "C X", 2 },
+    { "C Y", 3+3 },
+    { "C Z", 1+6 },
+};
+
+var total2 = lines.Sum(x => allCasesPart2[x]);
+
+Console.Out.WriteLine($"Part 2: {total2}");
