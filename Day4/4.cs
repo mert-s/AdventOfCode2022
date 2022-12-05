a1f6f922a8c48efb864daea44aceff72cac98aec
mert-s <mert-sahan@hotmail.com>
2022-12-05 22:36:50 +0000

    Days 4 and 5 (incomplete)

---
+++ Day4/4.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+var lines = System.IO.File.ReadAllLines(@"myInput.txt");
+
+var currentSum = 0;
+foreach (var line in lines)
+{
+    var items = line.Split(",").Select(x => (x.Split("-").Select(int.Parse)).ToArray()).ToArray();
+
+    if (items[0][0] <= items[1][0] &&
+        items[0][1] >= items[1][1])
+        currentSum += 1;
+    else if (items[0][0] >= items[1][0] &&
+        items[0][1] <= items[1][1])
+        currentSum += 1;
+}
+
+Console.WriteLine(currentSum);
+
+currentSum = 0;
+foreach (var line in lines)
+{
+    var items = line.Split(",").Select(x => (x.Split("-").Select(int.Parse)).ToArray()).ToArray();
+    //create array of 1st item, if 2nd item any of them match, then add 1 and continue
+    if (items[0][0] <= items[1][0] && items[0][1] >= items[1][0])
+        currentSum += 1;
+    else if (items[0][0] >= items[1][0] && items[0][0] <= items[1][1])
+        currentSum += 1;
+}
+
+Console.WriteLine(currentSum);
---
+++ Day5/5.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+var lines = System.IO.File.ReadAllLines(@"myInput.txt").ToList();
+
+var stackBottom = lines.First(x => x.Contains("1"));
+var index = lines.IndexOf(stackBottom);
+var stringIndices = stackBottom.Where(x => x != ' ').ToDictionary(x => stackBottom.IndexOf(x), x => (int)char.GetNumericValue(x));
+
+List<Stack<char>> stacks = Enumerable.Range(1, stringIndices.Count).Select(_ => new Stack<char>()).ToList();
+for (int i = index - 1; i >= 1; i--)
+{
+    var thisRow = lines[i];
+    for (int j = 0; j < thisRow.Length; j++)
+    {
+        if(char.IsLetter(thisRow[j]))
+            stacks[stringIndices[j]-1].Push(thisRow[j]);
+    }
+}
+
+//input stack parser above working, output currently broken
+foreach (var line in lines)
+{
+    if (line.StartsWith("move"))
+    {
+        var resultString = Regex.Matches(line, @"\d+").Select(x => int.Parse(x.Value)).ToArray();
+
+        for (int i = 0; i < resultString[0]; i++)
+        {
+            if(stacks[resultString[1]-1].TryPop(out var popped))
+                stacks[resultString[2]-1].Push(popped);
+        }
+    }
+}
+
+foreach (var stack in stacks)
+{
+    stack.TryPeek(out var item);
+    Console.WriteLine(item);
+}
+Console.WriteLine();
