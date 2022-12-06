4adfa157f5d6bd168c38704755e40361b1fef31a
mert-s <mert-sahan@hotmail.com>
2022-12-06 22:06:45 +0000

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = System.IO.File.ReadAllText(@"myInput.txt");
+
+static int GetFirstMarker(int distinctCharacters, string input)
+{
+    for (int i = distinctCharacters - 1; i < input.Length; i++)
+    {
+        if (new HashSet<char>(input[(i - distinctCharacters + 1)..(i+1)]).Count == distinctCharacters)
+            return i + 1;
+    }
+    return int.MaxValue;
+}
+
+Console.WriteLine($"Part 1: {GetFirstMarker(4, input)}");
+Console.WriteLine($"Part 1: {GetFirstMarker(14, input)}");
